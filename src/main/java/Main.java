feb8139a91e1f934d89a38ac16a9a8197032f2fa
Paul <paulhaeussler@gmx.de>
2020-05-14 21:49:08 +0200

    html server & jenkins setup test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,23 @@ public class Main {
 
 
     public static void main(String args[]){
-        System.out.println("SOOOOOOOOOOOOOOS");
+
+        String csvData = "Hier steht später JSON code";
+        //TODO Neuestes CSV finden
+
+        //TODO CSV lesen
+
+        //TODO CSV zu JSON
+
+        //Data als http server serven
+        try{
+            ServerHandler sh = new ServerHandler(csvData);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //Loop forever to keep running
+        while(true);
     }
 
     
---
+++ src/main/java/ServerHandler.java
@@ -0,0 +1,45 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class ServerHandler {
+
+    static final int PORT = 11555;
+    private MyHandler h;
+
+    public ServerHandler(String ServerInfo) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        h = new MyHandler(ServerInfo);
+        server.createContext("/data.json", h);
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    public void updateServerInfo(String newInfo){
+        h.serverInfo = newInfo;
+    }
+
+    static class MyHandler implements HttpHandler {
+
+        private String serverInfo;
+
+        public MyHandler(String ServerInfo){
+            super();
+            serverInfo = ServerInfo;
+        }
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = serverInfo;
+            t.getResponseHeaders().set("Access-Control-Allow-Origin", "http://nebular-insights.de");
+            t.sendResponseHeaders(200, 0);
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
